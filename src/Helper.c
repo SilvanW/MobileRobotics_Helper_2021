b628cf9c6fac4b8cdcd03eb1fe83f6e538523440
TimBarmettler4 <tim.barmettler@bluewin.ch>
2021-12-13 08:53:14 +0100

    Main Header

--- src/Helper.c
+++ src/Helper.c
@@ -1,3 +1,12 @@
+/* 
+* Helper.c
+* Main Programm für Gruppenarbeit des Semesterende
+* Autor: Silvan Wiedmer (Projektleiter), Ramona Ferri, Tim Barmettler (Praktikant)
+* Erstellungsdatum: 13.12.21
+* Version: 1.0 - Aufruf Export und CSV
+*/
+
+
 #include <stdio.h>
 #include "Functions/exportToCSV.h"
 
