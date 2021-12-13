62a6ceb27e37678be0ca2c94f2b8eb9f8d286bb8
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2021-12-13 22:27:58 +0100

    Reading of csv data and storing in struct array sucessfull

--- src/Functions/importFromCSV.c
+++ src/Functions/importFromCSV.c
@@ -9,6 +9,7 @@
 
 // Include the Required Libraries => Required for FILE
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 // Define Dataset Structure
@@ -16,30 +17,62 @@ typedef struct {
           char Date[50];
           char Time[50];
           char Theme[50];
-          char Points;
+          char Points[50];
 } Dataset;
 
-// Define Array of Dataset => DataArray
-Dataset DataArray[50];
-
-char char1[50];
-char char2[50];
-char char3[50];
-int percent;
+char buff[1024];
+int rowCount = 0;
+int valIndex = 0;
+Dataset values[999];
 
 // Define File Pointer
 FILE *fpt;
 
-char num[50];
-
 // Call Main Routine
 int main() {
           fpt = fopen("test.csv", "r");
-          
-          while(fscanf(fpt, "%c, %c, %c, %d", char1, char2, char3, &percent) == 2) {
-                    printf("%s, %s, %s, %d", char1, char2, char3, percent);
+
+          if (!fpt) {
+                    printf("Error Reading file");
+                    return 0;
+          }          
+
+          int i = 0;
+          while (fgets(buff, 1024, fpt)) {
+                    char *field = strtok(buff, ", ");
+                    valIndex = 0;
+                    while(field != NULL) {
+                              switch (valIndex) {
+                              case 0:
+                                        strcpy(values[rowCount].Date, field);
+                                        break;
+                              
+                              case 1:
+                                        strcpy(values[rowCount].Time, field);
+                                        break;
+
+                              case 2:
+                                        strcpy(values[rowCount].Theme, field);
+                                        break;
+
+                              case 3:
+                                        strcpy(values[rowCount].Points, field);
+                                        break;
+
+                              default:
+                                        break;
+                              }
+                              field = strtok(NULL, ", ");
+                              valIndex++;
+                    }
+
+                    rowCount++;
           }
-          
 
           fclose(fpt);
+
+          for(int i = 0; i < rowCount; i++) {
+                    printf("Daten: %s, %s, %s, %s", values[i].Date, values[i].Time, values[i].Theme, values[i].Points);
+                    printf("\n");
+          }   
 }
