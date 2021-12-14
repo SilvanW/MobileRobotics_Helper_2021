db239fe8be95aa47134a4f31b1d1a11dac15d583
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2021-12-15 00:36:55 +0100

    getTime Function => Get localtime and split in different components

--- src/Functions/getTime.c
+++ src/Functions/getTime.c
@@ -2,8 +2,27 @@
 #include <time.h>
 
 int main() {
-          time_t now;
-	now = time(0);
+          // Define Time variable
+          time_t s;
 
-          printf("Time %s \n", ctime(&now));
+          // Define Time struct
+          struct tm* current_time;
+          
+          // Set Time variable to NULL
+          s = time(NULL);
+          
+          // Set Current_Time to localtime from Time variable
+          current_time = localtime(&s);
+          
+          // Print values to console
+          printf("Day of the month = %d\n",current_time->tm_mday);
+          printf("Day in this year = %d\n",current_time->tm_yday);
+          printf("Day in this week = %d\n",current_time->tm_wday);
+          printf("Month of this year = %d\n",(current_time->tm_mon + 1));
+          printf("Current year = %d\n",(current_time->tm_year + 1900));
+          printf("hour:min:sec = %02d:%02d:%02d\n",current_time->tm_hour,current_time->tm_min,current_time->tm_sec);
+          
+          // return 0
+          return 0;
+
 }
