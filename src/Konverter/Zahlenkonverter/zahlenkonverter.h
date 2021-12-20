8391f071ec64a2318018bc201c360d1bd88435f0
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2021-12-20 14:33:28 +0100

    new files from seitz

--- src/Konverter/Zahlenkonverter/zahlenkonverter.h
+++ src/Konverter/Zahlenkonverter/zahlenkonverter.h
@@ -1,22 +1,38 @@
 #ifndef ZAHLENKONVERTER_H
 #define ZAHLENKONVERTER_H
 
-// Benötigte Libraries includen
+///////////////
+// Libraries //
+///////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <errno.h>
+
+
+//////////////////
+// Enumerations //
+//////////////////
 
 typedef enum
 {
-	EXIT,
-	DECIMAL,
-	BINARY,
-	HEXADECIMAL,
+	CONV_EXIT,
+	CONV_DECIMAL,
+	CONV_BINARY,
+	CONV_HEXADECIMAL,
+	CONV_OTHER,
+	CONV__COUNT,
 }
 ConverterTypes;
 
-// Funktionsprototyp
+
+////////////////////////////////
+// Public Function Prototypes //
+////////////////////////////////
+
 void NumberConverter(void);
+
 
 #endif
