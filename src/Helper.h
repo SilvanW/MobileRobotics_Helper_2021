68ebb679c2d78839f908a6dd347fba8b600ad7b3
Silvan Wiedmer <silvan.wiedmer@bluewin.ch>
2021-12-21 15:50:43 +0100

    enums for switch case in main file added

---
+++ src/Helper.h
@@ -0,0 +1,108 @@
+// Enumeration Hauptmenu
+typedef enum {
+    MATHEMATIK,
+    PHYSIK,
+    OPTIK,
+    ELEKTRONIK,
+    INFORMATIK,
+    CONVERTER,
+    SPIELE,
+    AUSWERTUNG,
+    EXIS      
+} Hauptmenue;
+
+// Enumeration Mathematik
+typedef enum {
+          MATHEMATIK_1,
+          MATHEMATIK_2
+} Mathematik;
+
+// Enumeration Mathematik_1
+typedef enum {
+          LINALG,
+          TRIGONOMETRISCHE_FUNKTIONEN,
+          QUIZ,
+          HANGMAN
+} Mathematik_1;
+
+// Enumeration Mathematik_2
+typedef enum {
+          EMPTY
+} Mathematik_2;
+
+// Enumeration Physik
+typedef enum {
+          PHYSIK_1,
+          PHYSIK_2
+} Physik;
+
+// Enumeration Physik_1
+typedef enum {
+          QUIZ
+} Physik_1;
+
+// Enumeration Physik_2
+typedef enum {
+          EMPTY
+} Physik_2;
+
+// Enumeration Optik
+typedef enum {
+          OPTIK_1,
+          OPTIK_2
+} Optik;
+
+// Enumeration Optik_1
+typedef enum {
+          FORMELN,
+          QUIZ,
+          SPIEGELSIMULATION
+} Optik_1;
+
+// Enumeration Optik_2
+typedef enum {
+          EMPTY
+} Optik_2;
+
+// Enumeration Elektronik
+typedef enum {
+          ELEKTRONIK_1,
+          ELEKTRONIK_2
+} Elektronik;
+
+// Enumeration Elektronik_1
+typedef enum {
+          QUIZ
+} Elektronik_1;
+
+// Enumeration Elektronik_2
+typedef enum {
+          EMPTY
+} Elektronik_2;
+
+// Enumeration Informatik
+typedef enum {
+          INFORMATIK_1,
+          INFORMATIK_2
+} Informatik;
+
+// Enumeration Informatik_1
+typedef enum {
+          QUIZ
+} Informatik_1;
+
+// Enumeration Informatik_2
+typedef enum {
+          EMPTY
+} Informatik_2;
+
+// Enumeration Converter
+typedef enum {
+          ZAHLENSYSTEM,
+          EINHEITEN,
+} Converter;
+
+// Enumeration Spiele
+typedef enum {
+          TICTACTOE
+} Spiele;
